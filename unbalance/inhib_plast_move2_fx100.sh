40746fa282f98d097ef36e62717d23a089a30d8d
alicegawa <fukuda@brain.imi.i.u-tokyo.ac.jp>
2015-11-18 14:47:18 +0900

    we can execute this program in fx100
    add the jobscript.

---
+++ unbalance/inhib_plast_move2_fx100.sh
@@ -0,0 +1,70 @@
+#!/bin/bash -x
+#
+#PJM -L "rscgrp=fx-debug"
+#PJM -L "node=30"
+#PJM -L "elapse=01:00:00"
+#PJM --mpi "proc=960"
+#PJM --stg-transfiles all
+#--PJM --mpi "use-rankdir"
+#PJM --fs "large2", "home"
+
+#PJM --stgin-basedir /home/usr8/z48928t/neuron_kplus
+
+#PJM --stgin "rank=* ./* %r:./"
+#PJM --stgin "rank=* ./data/* %r:./data/"
+#PJM --stgin "rank=* ./weights/* %r:./weights/"
+#PJM --stgin "rank=* ./result/* %r:./result/"
+#PJM --stgin "rank=* ./change/* %r:./change/"
+
+#PJM --stgin "rank=* /home/hp120263/k01792/neuron_kplus/specials/sparc64/special %r:./"
+#PJM --stgin "rank=* /home/hp120263/k01792/neuron_kplus/stgin/* %r:./"
+
+
+#PJM --stgout "rank=* %r:./result/* /large2/z48928t/inhib_plast/move/result/%j/"
+#PJM --stgout "rank=* %r:./change/* /large2/z48928t/inhib_plast/move/change/%j/"
+
+#PJM -s
+#PJM -m "e"
+#PJM --mail-list "fukuda@g.brain.imi.i.u-tokyo.ac.jp"
+#
+
+#. /work/system/Env_base
+
+NRNIV="/home/usr8/z48928t/neuron_kplus/specials/sparc64/special -mpi"
+#NRNIV="./special -mpi"
+HOC_NAME="./network.hoc"
+
+NRNOPT=\
+" -c INPUT_E=0.005"\
+" -c INPUT_I=0.01"\
+" -c OUT1_E=0.001"\
+" -c OUT1_I=0.01"\
+" -c OUT2_E=0.002"\
+" -c OUT2_I=0.01"\
+" -c IO_E=0.002"\
+" -c IO_I2E=0.001"\
+" -c IO_I2I=0.001"\
+" -c OUT1_SPON_E_K=0.15"\
+" -c OUT1_SPON_E_T=0.35"\
+" -c OUT2_SPON_E_K=0.13"\
+" -c OUT2_SPON_E_T=0.32"\
+" -c IN_SPON_K=0.15"\
+" -c IN_SPON_T=0.35"\
+" -c DOPAMINE=0.01"\
+" -c LEARNING_RATE=0.00025"\
+" -c LR4INPUT=0.00015"\
+" -c OUT1_SPON_I_K=0.15"\
+" -c OUT1_SPON_I_T=0.3"\
+" -c LTD=0.7"\
+" -c LTD4INPUT=1.3"\
+" -c LEARNING_RATE_I=0.0001"\
+" -c LR4INPUT_I=0.001"
+
+
+LPG="lpgparm -t 4MB -s 4MB -d 4MB -p 4MB"
+MPIEXEC="mpiexec -mca mpi_print_stats 1"
+PROF=""
+echo "${PROF} ${MPIEXEC} ${LPG} ${NRNIV} ${HOC_NAME}"
+time ${PROG} ${MPIEXEC} ${LPG} ${NRNIV} ${NRNOPT} ${HOC_NAME}
+
+sync
